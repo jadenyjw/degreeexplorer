7768f6ed9a52309fa090347d6c88a8af7aa83808
Jaden Wang <jadenyjw@gmail.com>
2018-01-01 22:29:16 -0500

    Add pem file for POC

--- poc.py
+++ poc.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 username = input("UTORID:")
 password = input("Password:")
 
-r = requests.get('https://degreeexplorer.utoronto.ca/degreeExplorer/', verify=False)
+r = requests.get('https://degreeexplorer.utoronto.ca/degreeExplorer/', verify='cert.pem')
 in_session = False
 new_url = ""
 jsession_id = ""
@@ -19,17 +19,14 @@ for char in r.url:
 
 jsession_id = jsession_id[12:]
 cookies = dict(JSESSIONID=jsession_id)
-r = requests.post(new_url, verify=False, data = {'j_username':username, 'j_password': password, '_eventId_proceed':''}, cookies=cookies)
+r = requests.post(new_url, verify='cert.pem', data = {'j_username':username, 'j_password': password, '_eventId_proceed':''}, cookies=cookies)
 req_cookies = r.cookies
 soup = BeautifulSoup(r.text, 'html.parser')
 saml = soup.find("input", {"name":"SAMLResponse"})['value']
-print(r.text)
 cookies = dict(DPSamlSpReqURL='/degreeExplorer/', redirectUrl='http://www.acorn.utoronto.ca/dxLogout.html')
-r = requests.post('https://degreeexplorer.utoronto.ca/spACS', verify=False, cookies=cookies, data = {'SAMLResponse': saml}, allow_redirects=False)
+r = requests.post('https://degreeexplorer.utoronto.ca/spACS', verify='cert.pem', cookies=cookies, data = {'SAMLResponse': saml}, allow_redirects=False)
 auth = r.cookies
-
-r = requests.get('https://degreeexplorer.utoronto.ca/degreeExplorer/rest/dxMenu/getStudentUserData', cookies=auth, verify=False)
+r = requests.get('https://degreeexplorer.utoronto.ca/degreeExplorer/rest/dxMenu/getStudentUserData', cookies=auth, verify='cert.pem')
 auth.update(r.cookies)
-print(auth)
-r = requests.get('https://degreeexplorer.utoronto.ca/degreeExplorer/rest/dxPlanner/getPlanner', cookies=auth, verify=False)
+r = requests.get('https://degreeexplorer.utoronto.ca/degreeExplorer/rest/dxPlanner/getPlanner', cookies=auth, verify='cert.pem')
 print(r.text)
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -8,9 +8,36 @@ import {
 } from 'react-native';
 
 export default class LoginScreen extends Component {
+
+  _login = (username, password) => {
+    const myRequest = new Request('https://acorn.utoronto.ca')
+    fetch(myRequest)
+    .then(response => {
+      if (response.status === 200) {
+        console.log(response);
+
+      }
+    })
+    .then(response => {
+      console.debug(response);
+      // ...
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+
+  constructor(props){
+    super(props);
+
+  }
+
   render() {
+
+    console.log("Login results:" + this._login("wangy841", "WordPass@123"));
     return (
-      <Text>LoginScreen</Text>
+      <Text>_login</Text>
+
     );
+
   }
 }
